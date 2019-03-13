13d1d363d13efbd0491378c184d57e2b8271e30c
Michael <mich561d@hotmail.com>
2019-03-13 19:23:43 +0100

    Added more test

--- src/test/java/junit/TCompany.java
+++ src/test/java/junit/TCompany.java
@@ -3,9 +3,14 @@ package junit;
 import dto.AddressDTO;
 import dto.CityInfoDTO;
 import dto.CompanyDTO;
+import entity.Address;
 import entity.Company;
+import entity.Phone;
+import facade.FCityInfo;
 import facade.FCompany;
+import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -16,7 +21,8 @@ import static org.junit.Assert.*;
  */
 public class TCompany {
 
-    FCompany facade = new FCompany(Persistence.createEntityManagerFactory("pu-test", null));
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu-test", null);
+    FCompany facade = new FCompany(emf);
 
     public void testGetCompanyByID() {
         Company c = facade.getCompanyByID(1);
@@ -54,15 +60,28 @@ public class TCompany {
     }
 
     public void testGetAllCompaniesWithNumEmployeesOver() {
-
+        List<CompanyDTO> cs = facade.getAllCompaniesWithNumEmployeesOver(360);
+        assertEquals(2, cs.size());
     }
 
     public void testGetAllCompaniesWithMarketValueOver() {
-
+        List<CompanyDTO> cs = facade.getAllCompaniesWithMarketValueOver(2500000);
+        assertEquals(1, cs.size());
     }
 
     public void testCreateCompany() {
-
+        List<CompanyDTO> csBefore = facade.getAllCompanies();
+        List<Phone> phones = new ArrayList();
+        Phone phone = new Phone("13371337", "Call the elite");
+        phones.add(phone);
+        Address address = new Address("Heaven", "Because we are gods", new FCityInfo(emf).getAllCitiesRaw().get(4));
+        Company company = new Company("The Turtle Troopers A/S", "We are the best", 36069420, 4, 2499999, "contact@TTT.com", phones, address);
+        address.getInfoEntities().add(company);
+        phone.setInfoEntity(company);
+        facade.createCompany(company);
+        facade.createCompany(company);
+        List<CompanyDTO> csAfter = facade.getAllCompanies();
+        assertEquals(csBefore.size() + 1, csAfter.size());
     }
 
     public void testUpdateCompany() {
