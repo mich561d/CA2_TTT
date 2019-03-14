504ff54f9119f63d55fe753e4bce5e259547121b
Michael <mich561d@hotmail.com>
2019-03-14 10:04:58 +0100

    Fixes

--- src/main/java/entity/CityInfo.java
+++ src/main/java/entity/CityInfo.java
@@ -18,30 +18,30 @@ import javax.persistence.OneToMany;
 @Entity
 @NamedQueries({
     @NamedQuery(name = "CityInfo.findAll", query = "SELECT c FROM CityInfo c")
-    , @NamedQuery(name = "CityInfoDTO.findAll", query = "SELECT NEW dto.CityInfoDTO(c.id, c.zipCode, c.city) FROM CityInfo c")
-    , @NamedQuery(name = "CityInfoDTO.findByZipCode", query = "SELECT NEW dto.CityInfoDTO(c.id, c.zipCode, c.city) FROM CityInfo c WHERE c.zipCode = :zipCode")
-    , @NamedQuery(name = "CityInfo.findAllZipCodes", query = "SELECT c.zipCode FROM CityInfo c")})
+    , @NamedQuery(name = "CityInfoDTO.findAll", query = "SELECT NEW dto.CityInfoDTO(c.id, c.zip, c.city) FROM CityInfo c")
+    , @NamedQuery(name = "CityInfoDTO.findByZipCode", query = "SELECT NEW dto.CityInfoDTO(c.id, c.zip, c.city) FROM CityInfo c WHERE c.zip = :zip")
+    , @NamedQuery(name = "CityInfo.findAllZipCodes", query = "SELECT c.zip FROM CityInfo c")})
 public class CityInfo implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String zipCode, city;
+    private String zip, city;
     @OneToMany(mappedBy = "cityInfo", cascade = CascadeType.ALL)
     private List<Address> addresses;
 
     public CityInfo() {
     }
 
-    public CityInfo(String zipCode, String city, List<Address> addresses) {
-        this.zipCode = zipCode;
+    public CityInfo(String zip, String city, List<Address> addresses) {
+        this.zip = zip;
         this.city = city;
         this.addresses = addresses;
     }
 
-    public CityInfo(String zipCode, String city) {
-        this.zipCode = zipCode;
+    public CityInfo(String zip, String city) {
+        this.zip = zip;
         this.city = city;
     }
 
@@ -53,12 +53,12 @@ public class CityInfo implements Serializable {
         this.id = id;
     }
 
-    public String getZipCode() {
-        return zipCode;
+    public String getZip() {
+        return zip;
     }
 
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
+    public void setZip(String zip) {
+        this.zip = zip;
     }
 
     public String getCity() {
