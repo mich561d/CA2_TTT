15367635d2c24163bb20c65fdadc3c5cc3d5050b
mf226 <cph-mf226@cphbusiness.dk>
2019-03-14 10:11:26 +0100

    persistence added entities to pu2

--- src/main/java/tester/TestPU.java
+++ src/main/java/tester/TestPU.java
@@ -5,6 +5,7 @@ import entity.CityInfo;
 import entity.Hobby;
 import entity.Person;
 import entity.Phone;
+import generateData.Generator2;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -23,8 +24,10 @@ public class TestPU {
     public static void main(String[] args) {
         // TODO code application logic here
         Persistence.generateSchema("pu-test", null);
-
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        Generator2 generator = new Generator2(emf);
+        List<Person> persons = generator.generateRandomPersons(50);
+        
 //        EntityManager em = emf.createEntityManager();
 //        List<Hobby> hobbies = new ArrayList();
 //        hobbies.add(new Hobby("Programming", "Java is great"));
