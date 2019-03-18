2674beeb37583e52ac21cd3e57670641d2f45b68
mf226 <cph-mf226@cphbusiness.dk>
2019-03-18 11:43:21 +0100

    adjusted corsResponseFilter

--- src/main/java/cors/CorsResponseFilter.java
+++ src/main/java/cors/CorsResponseFilter.java
@@ -14,8 +14,8 @@ public class CorsResponseFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestCtx, ContainerResponseContext res) throws IOException {
         res.getHeaders().add("Access-Control-Allow-Origin", "*");
+        res.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization,x-access-token");
         res.getHeaders().add("Access-Control-Allow-Credentials", "true");
         res.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, HEAD");
-        res.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization"); // ,x-access-token
     }
 }
