d7799aa788fec6a5f590dcbf94b0dbcbb88f7c2a
Christian <rygerlunger@gmail.com>
2019-03-18 09:37:40 +0100

    Updatede Tperson and PhoneDTO

--- src/test/java/junit/TPerson.java
+++ src/test/java/junit/TPerson.java
@@ -1,16 +1,16 @@
 package junit;
 
 import dto.AddressDTO;
-import dto.CityInfoDTO;
 import dto.HobbyDTO;
 import dto.PersonDTO;
 import dto.PhoneDTO;
-import entity.Company;
+import entity.Address;
+import entity.Hobby;
 import entity.Person;
 import entity.Phone;
-import static entity.db.InfoEntity_.email;
-import facade.FCompany;
+import facade.FCityInfo;
 import facade.FPerson;
+import facade.FPhone;
 import generateData.Generator2;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,29 +18,26 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  *
  * @author Michael, Christian
  */
 public class TPerson {
+    
+    private static EntityManagerFactory emf;
+    private static FPerson facade;
+    private static List<Person> persons;
 
-    FPerson facade;
-
-    @Before
-    public void setup() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu-test");
-        facade = new FPerson(emf);
+    @BeforeClass
+    public static void setupClass() {
+        emf = Persistence.createEntityManagerFactory("pu-test");
         Generator2 generator = new Generator2(emf);
-        List<Person> persons = generator.generateExactTestPersonData();
-        List<Company> companies = generator.generateExactTestCompanyData();
+        persons = generator.generateExactTestPersonData();
+        facade = new FPerson(emf);
         for (int i = 0; i < persons.size(); i++) {
             facade.createPerson(persons.get(i));
-        }
-        FCompany fCompany = new FCompany(emf);
-        for (int i = 0; i < companies.size(); i++) {
-            fCompany.createCompany(companies.get(i));
         }
     }
 
@@ -55,21 +52,22 @@ public class TPerson {
     public void getPersonByEmail() {
         Person p = facade.getPersonByIDRaw(5);
         PersonDTO person = facade.getPersonByEmail(p.getEmail());
-        assertNotNull(email);
+        assertNotNull(person.getEmail());
 
     }
 
     @Test
     public void getPersonByPhone() {
-        Person p = facade.getPersonByIDRaw(7);
-        PersonDTO phone = facade.getPersonByPhone((PhoneDTO) p.getPhones());
-        assertNotNull(phone);
+        String number = "12435687";
+        PhoneDTO phone = new PhoneDTO(number, "Mobile");
+        PersonDTO person = facade.getPersonByPhone(phone);
+        assertNotNull(person);
     }
 
     @Test
     public void getAllPersons() {
-        List<PersonDTO> persons = facade.getAllPersons();
-        assertEquals(100, persons.size());
+        List<PersonDTO> personas = facade.getAllPersons();
+        assertEquals(20, personas.size());
     }
 
     @Test
@@ -78,27 +76,29 @@ public class TPerson {
         assertEquals(9, person.size());
     }
 
-    @Test
-    public void getAllPersonsByCity() {
-        List<PersonDTO> person = facade.getAllPersonsByCity(new CityInfoDTO(14, "2600", "Glostrup"));
-        assertEquals(10, person.size());
-
-    }
+//    @Test
+//    public void getAllPersonsByCity() {
+//        List<PersonDTO> person = facade.getAllPersonsByCity(new CityInfoDTO(14, "2600", "Glostrup"));
+//        assertEquals(10, person.size());
+//
+//    }
 
     @Test
     public void getAllPersonsByAddress() {
         List<PersonDTO> person = facade.getAllPersonsByAddress(new AddressDTO(1, "Personstreet", "Number: 0"));
-        assertEquals(1, person.size());
+        assertEquals(20, person.size());
     }
 
     @Test
     public void createPerson() {
         List<PersonDTO> csBefore = facade.getAllPersons();
-        List<HobbyDTO> hobby = new ArrayList();
+        List<Hobby> hobby = new ArrayList();
         List<Phone> phones = new ArrayList();
         Phone phone = new Phone("12345678", "Call the test person");
+        phones.add(phone);
+        Address address = new Address("Testervej 1", "Test personer bor her", new FCityInfo(emf).getAllCitiesRaw().get(4));
         
-        //Person p = new Person("Test", "Testsen", hobby, "test@test.dk", "phone", "Testervej 1");
+        Person p = new Person("Test", "Testsen", hobby, "test@test.dk", phones, address);
     }
 
     @Test
